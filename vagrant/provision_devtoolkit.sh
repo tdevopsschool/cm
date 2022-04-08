ebfd080b323b105a4d578de1681b120492763a7e
Eduard Miachkov <eduard.miachkov@t-systems.com>
2022-04-08 16:27:19 +0300

    Add devtoolkit configuration

---
+++ vagrant/provision_devtoolkit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+echo "Install wget, ant, maven, gradle"
+sudo yum install wget
+sudo yum install ant
+sudo yum install maven
+#curl -s "https://get.sdkman.io" | bash
+#source "/home/vagrant/.sdkman/bin/sdkman-init.sh"
+#sdk install gradle 
+
+echo "Install docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose || true
+docker-compose --version
